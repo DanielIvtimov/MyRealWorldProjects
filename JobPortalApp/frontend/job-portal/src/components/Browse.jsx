934f7affa5a01c133f71415c68c31ca2b517f683
Daniel <danieliftimov3@gmail.com>
2025-08-26 16:26:10 +0200

    Created Browse page with showing total number of jobs found

--- JobPortalApp/frontend/job-portal/src/components/Browse.jsx
+++ JobPortalApp/frontend/job-portal/src/components/Browse.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Navbar from './shared/Navbar'
+import Job from './Job';
+
+const randonJobs = [1,2,3,4,5,6,7];
 
 const Browse = () => {
   return (
-    <div>Browse</div>
+    <div>
+        <Navbar />
+        <div className='max-w-7xl mx-auto my-10'>
+            <h1 className="font-bold text-xl my-10">Search Results ({randonJobs.length})</h1>
+            <div className='grid grid-cols-3 gap-4'>
+                {
+                    randonJobs.map((item, index) => {
+                        return (
+                            <Job />
+                        )
+                    })
+                }
+            </div>
+        </div>
+    </div>
   )
 }
 
