086aa4020e2d4b19b7da2b9c57ae24cf66813a57
Daniel <danieliftimov3@gmail.com>
2025-12-01 21:44:07 +0100

    Registering Note model and creating first notes

--- NotesApp/backend/noteapp/views.py
+++ NotesApp/backend/noteapp/views.py
@@ -6,14 +6,14 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 
 # Create your views here.
-@api_view(["GET, POST,"])
-def notes (reqeust):
-    if reqeust.method == "GET":
+@api_view(["GET", "POST"])
+def notes (request):
+    if request.method == "GET":
         notes = Note.objects.all()
-        serializer = NoteSerializer(notes, many=true)  
+        serializer = NoteSerializer(notes, many=True)  
         return Response(serializer.data)
-    elif reqeust.method == "POST":
-        serializer = NoteSerializer(data=reqeust.data)
+    elif request.method == "POST":
+        serializer = NoteSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
