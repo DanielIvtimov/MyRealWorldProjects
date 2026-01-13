f8f164f3e940039e0b37c531ee4e0c2b8721008f
DanielIvtimov <danieliftimov3@gmail.com>
2026-01-13 10:57:51 +0100

    Implement Product session with table's, migrations, blade

--- laravel_online_shop/resources/views/admin/layouts/app.blade.php
+++ laravel_online_shop/resources/views/admin/layouts/app.blade.php
@@ -13,6 +13,8 @@
 
 		<link rel="stylesheet" href="{{ asset('admin-assets/plugins/dropzone/min/dropzone.min.css') }}">
 
+		<link rel="stylesheet" href="{{ asset('admin-assets/plugins/summernote/summernote.min.css') }}">
+
 		<link rel="stylesheet" href="{{ asset('admin-assets/css/custom.css') }}">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 	</head>
@@ -85,12 +87,20 @@
 		<script src="{{ asset('admin-assets/js/adminlte.min.js') }}"></script>
 
 		<script src="{{ asset('admin-assets/plugins/dropzone/min/dropzone.min.js') }}"></script>
+
+		<script src="{{ asset('admin-assets/plugins/summernote/summernote.min.js') }}"></script>
 		<!-- AdminLTE for demo purposes -->
 		<script src="{{ asset('admin-assets/js/demo.js') }}"></script> 
 
 		<script>
 			$.ajaxSetup({
 				headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
+			});
+
+			$(document).ready(function(){
+				$(".summernote").summernote({
+					height: 250,
+				});
 			});
 		</script>
 
--- laravel_online_shop/routes/web.php
+++ laravel_online_shop/routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\TempImagesController;
 use App\Http\Controllers\admin\SubCategoryController;
 use App\Http\Controllers\admin\BrandController;
+use App\Http\Controllers\admin\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
@@ -58,6 +59,9 @@ Route::group(['prefix' => 'admin'], function(){
         Route::get('/brands/{brand}/edit', [BrandController::class, 'edit'])->name('brands.edit');
         Route::put('/brands/{brand}', [BrandController::class, 'update'])->name('brands.update');
         Route::delete('/brands/{brand}', [BrandController::class, 'destroy'])->name('brands.delete');
+
+        // Product Routes
+        Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
         
         Route::post('/upload-temp-image', [TempImagesController::class, 'create'])->name('temp-images.create');
 
