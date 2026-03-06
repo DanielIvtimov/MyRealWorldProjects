dc522e7d23ad20a0c0df923c7e2726b8033b00fe
DanielIvtimov <danieliftimov3@gmail.com>
2026-03-06 14:18:34 +0100

    Update Admin Dashboard with orders, customers, products and sales

--- laravel_online_shop/app/Http/Controllers/admin/HomeController.php
+++ laravel_online_shop/app/Http/Controllers/admin/HomeController.php
@@ -5,14 +5,57 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
-        // $admin = Auth::guard('admin')->user();
-        // echo "Welcome ".$admin->name." <a href='".route('admin.logout')."'>Logout</a>"; 
+
+        $totalOrders = Order::where('status', '!=', 'cancelled')->count();
+        $totalProducts = Product::count();
+        $totalCustomers = User::where('role', '1')->count(); 
+
+        $totalRevenue = Order::where('status', '!=', 'cancelled')->sum('grand_total');
+
+        // This month revenue
+        $startOfMonth = Carbon::now()->startOfMonth()->format('Y-m-d');
+        $currentDate = Carbon::now()->format('Y-m-d');
+
+        $renevueThisMonth = Order::where('status', '!=', 'cancelled')
+            ->whereBetween('created_at', [$startOfMonth, $currentDate])
+            ->sum('grand_total');
+
+        // Last month revenue
+        $startOfLastMonth = Carbon::now()->subMonth()->startOfMonth()->format('Y-m-d');
+        $currentDate = Carbon::now()->subMonth()->format('Y-m-d');
+        $lastMonthName = Carbon::now()->subMonth()->format('M');
+
+        $renevueLastMonth = Order::where('status', '!=', 'cancelled')
+            ->whereBetween('created_at', [$startOfLastMonth, $currentDate])
+            ->sum('grand_total');
+
+        // Last 30 days revenue
+        $startOf30Days = Carbon::now()->subDays(30)->format('Y-m-d');
+        $currentDate = Carbon::now()->format('Y-m-d');
+
+        $renevueLast30Days = Order::where('status', '!=', 'cancelled')
+            ->whereBetween('created_at', [$startOf30Days, $currentDate])
+            ->sum('grand_total');
+
+        return view('admin.dashboard', compact(
+            'totalOrders',
+            'totalProducts',
+            'totalCustomers',
+            'totalRevenue', 
+            'renevueThisMonth', 
+            'renevueLastMonth', 
+            'renevueLast30Days', 
+            'lastMonthName',
+        ));
     }
     public function logout()
     {
