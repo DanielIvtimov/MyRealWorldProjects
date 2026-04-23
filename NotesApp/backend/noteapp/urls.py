2b12fd43c35863b2d96850d0b2b98c983fbae554
DanielIvtimov <danieliftimov3@gmail.com>
2026-04-23 13:30:25 +0200

    Deleting Stuff's

--- NotesApp/backend/noteapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from .import views
-
-urlpatterns = [
-    path("notes/", views.notes, name="notes"),
-    path("notes/<slug:slug>/", views.note_details, name="note-detail"),
-
-]
-
